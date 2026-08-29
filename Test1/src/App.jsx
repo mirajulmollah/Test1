7b2fd887c1dec56f0e1cdcb99e9b34d69fd96bc7
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-08-29 19:48:47 +0530

    all ok

---
+++ Test1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <button className='bg-black text-white px-1 py-2 rounded-2xl border-white border-2'>show popup</button>
+
+    </div>
+  )
+}
+
+export default App
