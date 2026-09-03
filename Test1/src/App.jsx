9cda560a14ce8cf3fc08e3e1e3546e3e5bdb2bda
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-09-03 20:37:08 +0530

    first commit

--- Test1/src/App.jsx
+++ Test1/src/App.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react'
-
-
+import { useState } from "react"
 
 function App() {
-  const [popup,setpopup]=useState(false)
+
+  const[name,setname]=useState('')
+  const[email,setEmail]=useState('')
+
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
-      <button onClick={()=>{
-        setpopup(true)
-      }} className='bg-black text-white px-1 py-2 rounded-2xl border-white border-2'>show popup</button>
-      
-      {popup &&
-      (<div className='h-screen w-screen bg-black/50 absolute top-0 left-0'></div>)}
+    <>
+    <input type='text' placeholder="Enter Your text" value={name} className="border mt-2 ml-2" onChange={(e)=>{
 
-      <button onClick={()=>{
-        setpopup(false)
-      }} className='absolute top-2 right-5 bg-black px-1 py-1'>❌</button>
+      setname(e.target.value)
+      
 
-      <div className='h-full w-full max-h-[20rem] max-w-[40rem] bg-[#242424] absolute flex justify-center items-center flex-col gap-5'>
-        
-        <h1 className='font-bold text-white'>⚠️ Confirm</h1>
-        <p className='font-bold text-white'>Are you sure you want to delete this??</p>
-        <div className=' flex gap-10'>  
-        <button className='bg-black text-white px-3 py-2 rounded-2xl border-white border-2'>Yes</button>
-        <button className='bg-black text-white px-3 py-2 rounded-2xl border-white border-2'>No</button>
-       
-        </div>
+    }}/>
+    <h1>{name}</h1>
+    <br></br>
+
+    <input type='text' placeholder="Enter Your email" value={email} className="border mt-2 ml-2" onChange={(e)=>{
+
+      setEmail(e.target.value)
       
-      </div>
-      
-    </div>
+
+    }}/>
+
+    
+    <h1>{email}</h1>
+    <br></br>
+    <button onClick={()=>{
+      setname("")
+      setEmail('')
+    }} className="border">Click Me</button>
+    
+
+    </>
   )
 }
-
 export default App
