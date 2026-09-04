af59f8e2ff9462e7314254edd30addc8dc101aab
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-09-04 20:23:51 +0530

    first commit

--- Test1/src/App.jsx
+++ Test1/src/App.jsx
@@ -2,35 +2,59 @@ import { useState } from "react"
 
 function App() {
 
-  const[name,setname]=useState('')
-  const[email,setEmail]=useState('')
+  const [data, setdata] = useState({
+    name: '',
+    email: ''
+  })
+  function nameHandeller(e) {
+    setdata({ ...data, name: e.target.value })
+  }
+
+  function emailHandeler(e) {
+    setdata({ ...data, email: e.target.value })
+  }
+
+  function inputhandeler(e) {
+    // const name=e.target.name
+    // const value=e.target.value
+    //destucture kore nilam
+    const { name, value } = e.target
+    // setdata({}) state vitor {} karon ata object asbe er vitor
+
+    setdata({ ...data, [name]: value })
+  }
+
 
   return (
     <>
-    <input type='text' placeholder="Enter Your text" value={name} className="border mt-2 ml-2" onChange={(e)=>{
+      <input type='text'
+        placeholder="Enter Your text"
+        value={data.name}
+        className="border mt-2 ml-2"
+        name='name'
+        onChange={inputhandeler} />
+      <h1>{data.name}</h1>
+      <br></br>
 
-      setname(e.target.value)
-      
+      <input type='text'
+        placeholder="Enter Your email"
+        value={data.email}
+        className="border mt-2 ml-2"
+        name='email'
+        onChange={inputhandeler} />
 
-    }}/>
-    <h1>{name}</h1>
-    <br></br>
 
-    <input type='text' placeholder="Enter Your email" value={email} className="border mt-2 ml-2" onChange={(e)=>{
+      <h1>{data.email}</h1>
+      <br></br>
+      <button onClick={(e) => {
+        setdata({
+          name: '',
+          email: ''
+        })
 
-      setEmail(e.target.value)
-      
 
-    }}/>
+      }} className="border">Click Me</button>
 
-    
-    <h1>{email}</h1>
-    <br></br>
-    <button onClick={()=>{
-      setname("")
-      setEmail('')
-    }} className="border">Click Me</button>
-    
 
     </>
   )
