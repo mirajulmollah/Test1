32819bbacc808721c20005cb943123127f0dba2e
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-09-05 20:25:32 +0530

    first commit

--- Test1/src/App.jsx
+++ Test1/src/App.jsx
@@ -2,61 +2,69 @@ import { useState } from "react"
 
 function App() {
 
-  const [data, setdata] = useState({
-    name: '',
-    email: ''
-  })
-  function nameHandeller(e) {
-    setdata({ ...data, name: e.target.value })
-  }
+  const [language, setlanguage] = useState(
+    {
+      html: false,
+      css: false,
+      js: false,
+    }
+  )
 
-  function emailHandeler(e) {
-    setdata({ ...data, email: e.target.value })
-  }
 
-  function inputhandeler(e) {
-    // const name=e.target.name
-    // const value=e.target.value
-    //destucture kore nilam
-    const { name, value } = e.target
-    // setdata({}) state vitor {} karon ata object asbe er vitor
-
-    setdata({ ...data, [name]: value })
-  }
 
 
   return (
-    <>
-      <input type='text'
-        placeholder="Enter Your text"
-        value={data.name}
-        className="border mt-2 ml-2"
-        name='name'
-        onChange={inputhandeler} />
-      <h1>{data.name}</h1>
-      <br></br>
-
-      <input type='text'
-        placeholder="Enter Your email"
-        value={data.email}
-        className="border mt-2 ml-2"
-        name='email'
-        onChange={inputhandeler} />
+    <div className="select-none">
 
 
-      <h1>{data.email}</h1>
-      <br></br>
-      <button onClick={(e) => {
-        setdata({
-          name: '',
-          email: ''
-        })
+      <label htmlFor="cb">
+        <input type="checkbox" id='cb' checked={language.html} onChange={(e) => {
+          setlanguage({
+            ...language,
+            html: e.target.checked
+          })
+        }} />
+        HTML
+
+      </label>
+
+      <label htmlFor="cb">
+        <input type="checkbox" id='cb' checked={language.css} onChange={(e) => {
+          setlanguage({
+            ...language,
+            css: e.target.checked
+          })
+        }} />
+        CSS
+
+      </label>
+
+      <label htmlFor="cb">
+        <input type="checkbox" id='cb' checked={language.js} onChange={(e) => {
+          setlanguage({
+            ...language,
+            js: e.target.checked
+          })
+        }} />
+        JS
+
+      </label>
+
+      <br />
+      <br />
+
+      {language.css && 'css clicked'}
+      <br />
+      <br />
+      {language.js && 'js clicked'}
+      <br />
+      <br />
+      {language.html && 'html clicked'}
 
 
-      }} className="border">Click Me</button>
 
 
-    </>
+    </div>
   )
 }
 export default App
