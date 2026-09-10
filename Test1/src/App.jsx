2e9c1f1d95d51b2c803e7daa25e4b54dd9408d58
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-09-10 21:39:24 +0530

    first commit

--- Test1/src/App.jsx
+++ Test1/src/App.jsx
@@ -2,33 +2,55 @@ import React, { useState } from 'react'
 
 function App() {
 
-  const [language,setLanguage] = useState({
-    html:false,
-    css:false,
+  const [language, setLanguage] = useState({
+    html: false,
+    css: false,
   })
 
+  function onHandle(e) {
+    
+   const key=(e.target.name)
+   setLanguage({
+    ...language,
+    [key]:e.target.checked
+   })
 
+
+  }
+
+  const SA =Object.values(language).every(items=> items===true)
+  // console.log(SA)
 
 
   return (
     <div>
       <div className='select-none'>
-  <label htmlFor="html">
-    <input type="checkbox" id="html" checked={language.html} onChange={(e) => setLanguage({
-      ...language,
-      html:e.target.checked})} />
-    HTML
-  </label>
 
-  <label htmlFor="css">
-    <input type="checkbox" id="css" checked={language.css} onChange={(e) => setLanguage({
-      ...language,
-      css:e.target.checked})} />
-    CSS
-  </label>
-</div>
+        <label htmlFor="sa">
+          <input type="checkbox" id="sa" 
+          checked={SA} 
+          onChange={(e)=>{
+            setLanguage({
+              html:e.target.checked,
+              css:e.target.checked
+            })
+          }} 
+          />
+          Select All
+        </label>
 
-      
+        <label htmlFor="html">
+          <input type="checkbox" id="html" name="html" checked={language.html} onChange={onHandle} />
+          HTML
+        </label>
+
+        <label htmlFor="css">
+          <input type="checkbox" id="css" name="css" checked={language.css} onChange={onHandle} />
+          CSS
+        </label>
+      </div>
+
+
     </div>
   )
 }
